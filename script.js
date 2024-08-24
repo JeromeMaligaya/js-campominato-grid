fa6be1a4ce1b243066a71662280c717769d9e15f
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-25 01:00:30 +0200

    recupero l'elemento dal Dom

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 //Check link
 console.log('JS OK')
 
-// 1.recupero l'elemento dal Dom
+// 1.recupero l'elemento dal Dom (grid, button)
+const grid = document.getElementById('grid');
+const button = document.querySelector('header button');
 
-//2.raccolta dati
-    
+//2.raccolta dati (grid values)
+
 // 3.lavorazione dati
+// evento al bottone
+    // ciclo for
+        // creo a monte funz createCell
+        // invoco la funz createCell
 
-// 4.generazione output
+        // numerare le celle [i]
+
+    // 4.generazione output
+    // stampo in pagina
